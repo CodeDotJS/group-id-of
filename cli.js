98dbd66bba36087f73689d6a60919bb6c91afc3e
CodeDotJS <rushforlinux@gmail.com>
2016-08-06 09:23:02 +0530

    clean

--- cli.js
+++ cli.js
@@ -2,84 +2,60 @@
 
 'use strict';
 
+const dns = require('dns');
 const https = require('https');
-
+const logUpdate = require('log-update');
 const colors = require('colors/safe');
 
-const argv = require('yargs')
+const arg = process.argv[2];
+const arrow = colors.cyan.bold('›');
 
-	.usage(colors.cyan.bold('\n Usage: $0 <command> [target]'))
-
-	.command('u', colors.cyan.bold(' ❱ ') + ' find facebook group\'s group id')
-
-	.demand(['u'])
-
-	.describe('u' , '❱ group\'s name')
-
-	.example(colors.green.bold('\n$0 -u startuptalkygroup'))
-
-	.argv;
+if (!arg || arg === '-h' || arg === '--help') {
+	console.log(`
+ ${colors.cyan('Usage   :')} group-id-of ${colors.blue('<group\'s name>\n')}
+ ${colors.cyan('Example :')} group-id-of ${colors.yellow('linuxscreenshots\n')}
+ ${colors.cyan('Help    :')} group-id-of ${colors.green('-h')} ${colors.dim('--help')}
+ `);
+	process.exit(1);
+}
 
 const updateNotifier = require('update-notifier');
-
 const pkg = require('./package.json');
 
 updateNotifier({pkg}).notify();
 
+const userArgs = arg;
+const pathReq = `/groups/${userArgs}`;
+
 const options = {
 	hostname: 'www.facebook.com',
-
 	port: 443,
-
-	path: '/groups/' + argv.u,
-
+	path: pathReq,
 	method: 'GET',
-
 	headers: {
 		'accept': 'text/html,application/json,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-
 		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36',
-
 		'Host': 'www.facebook.com',
-
 		'Connection': 'Keep-Alive',
-
 		'Accept-Language': 'en-GB,en-US;q=0.8,en;q=0.6'
 	}
 };
 
-function checkInternet(cb) {
-	require('dns').lookup('facebook.com', err => {
-		if (err && err.code === 'ENOTFOUND') {
-			cb(false);
-		} else {
-			cb(true);
-		}
-	});
-}
-
-// checking internet connection
-checkInternet(isConnected => {
-	if (isConnected) {
-		// do nothing : don't want to show a lot of messages.
-	} else {
-		// stop the whole process if the network is unreachable
-		console.log(colors.red.bold('\n ❱ Internet Connection   :   ✖\n'));
-
+dns.lookup('facebook.com', err => {
+	if (err && err.code === 'ENOTFOUND') {
+		logUpdate(`\n${colors.red.bold(arrow)} Please check your internet connection`);
 		process.exit(1);
+	} else {
+		logUpdate(`\n${arrow} ${colors.dim('Fetching Group\'s ID. Please wait!')}`);
 	}
 });
 
 const req = https.request(options, res => {
 	if (res.statusCode === 200) {
-		// don't want to show anything.
-	} else {
-		// just exit
-
-		process.exit(1);
+		logUpdate(`${colors.cyan.bold('\n›')} ${colors.yellow(arg)} ${colors.dim(`is a facebook group!`)}\n`);
 	}
-	let store = '';
 
+	let store = '';
 	res.setEncoding('utf8');
 
 	res.on('data', d => {
@@ -88,13 +64,13 @@ const req = https.request(options, res => {
 
 	res.on('end', () => {
 		const rePattern = new RegExp(/entity_id":"\d*/);
-
 		const arrMatches = store.match(rePattern);
 
 		if (arrMatches && arrMatches[0]) {
-			console.log(colors.cyan.bold('\n ❱ Group ID        : '), colors.green.bold(arrMatches[0].replace('entity_id":"', ''), '\n'));
+			logUpdate();
+			console.log(`${colors.cyan.bold(`${arrow}`)} ${colors.dim('Group ID of')} ${arg} ${colors.dim('is')} ${arrMatches[0].replace('entity_id":"', '')}\n`);
 		} else {
-			console.log(colors.red.bold('\n ❱ Facebook Group  :  ✖\n'));
+			logUpdate(`${colors.cyan.bold('\n›')} ${colors.dim(`Sorry "${arg}" is not a facebook group!`)}\n`);
 		}
 	});
 });
